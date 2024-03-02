d49bff0c316dafaf48da7ceaed8f30183c7f83eb
yonghee <yonghee@yongheeui-MacBookPro.local>
2024-03-02 16:12:28 +0900

    ntercepting routing

--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -6,7 +6,6 @@ type Props = { children: ReactNode, modal: ReactNode }
 export default function Layout({ children, modal }: Props) {
     return (
         <div className={styles.container}>
-            비포 로그인 레이아웃
             {children}
             {modal}
         </div>
