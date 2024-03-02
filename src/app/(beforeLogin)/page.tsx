86655e6a198a15b1e7b4a602947e83b218dfb264
yonghee <yonghee@yongheeui-MacBookPro.local>
2024-03-02 17:45:36 +0900

    main component

--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -1,24 +1,9 @@
-// 'use client'
 
-import styles from "@/app/page.module.css";
-import Image from "next/image";
-import xLogo from '../../../public/xlogo.png'
-import Link from "next/link";
+import Main from "./_component/Main";
 
-export default function Main() {
+export default function Home() {
   return (
-    <>
-      <div className={styles.left}>
-        <Image src={xLogo} alt="logo" />
-      </div>
-      <div className={styles.right}>
-        <h1>지금 일어나고 있는 일</h1>
-        <h2>지금 가입하세요.</h2>
-        <Link href="/i/flow/signup" className={styles.signup}>계정 만들기</Link>
-        <h3>이미 트위터에 가입하셨나요?</h3>
-        <Link href="/login" className={styles.login}>로그인</Link>
-      </div>
-    </>
+    <Main />
   )
 }
 
