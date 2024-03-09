c6735b110b3fa2c4e464708c6751ec93304637d3
yonghee <yonghee@yongheeui-MacBookPro.local>
2024-03-09 21:01:42 +0900

    readme

--- src/app/(beforeLogin)/_component/Main.tsx
+++ src/app/(beforeLogin)/_component/Main.tsx
@@ -1,4 +1,4 @@
-import styles from "@/app/page.module.css";
+import styles from "@/app/(beforeLogin)/_component/main.module.css";
 import Image from "next/image";
 import xLogo from '../../../../public/xlogo.png'
 import Link from "next/link";
